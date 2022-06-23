eaf7777c5796a2541b68402d13f8b3cfc5b2ade8
panorama713 <yaiba713@gmail.com>
2022-06-23 17:46:55 +0900

    first commit

---
+++ omym-backend/src/main/java/com/example/omymbackend/OmymBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.omymbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmymBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OmymBackendApplication.class, args);
+    }
+
+}
---
+++ omym-backend/src/test/java/com/example/omymbackend/OmymBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.omymbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmymBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
