b521d3fb615e4cc5c5507a2e66219e6d78683a86
iooe9094 <99378766+iooe9094@users.noreply.github.com>
2022-06-27 16:24:01 +0900

    메인 공통 파일 생성

---
+++ src/main/java/com/example/omymbackend/model/login.java
@@ -0,0 +1,15 @@
+package com.example.omymbackend.model;
+
+/**
+ * packageName : com.example.omymbackend.model
+ * fileName : login
+ * author : macbook
+ * date : 6/27/22
+ * description :
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 6/27/22         macbook          최초 생성
+ */
+public class login {
+}
